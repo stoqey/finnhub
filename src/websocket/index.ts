31723d7ecc36e0530425a4e6f33e47196b4a22ea
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-11-06 14:46:20 -0500

    use native events minor

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -1,3 +1,4 @@
+import { EventEmitter } from "events";
 import WebSocket from "ws";
 import { FINNHUB_KEY, TZ_ON } from "../config";
 import { TickData } from "../interface";
@@ -5,15 +6,13 @@ import { checkIfMarketIsOpen } from "../utils/checkIfMarketIsOpen";
 import { log } from "../utils/log";
 import JSONDATA from "../utils/text.utils";
 
-interface PublisherEvents {
-  /**
-   * Called when WS is ready
-   */
-  onReady: () => Promise<any>;
-
-  onData: (data: TickData) => Promise<any>;
-
-  error: (error: Error) => Promise<any>;
+/**
+ * Finnhub websocket events
+ */
+export enum FinnhubWSEvents {
+  onData = "onData",
+  onReady = "onReady",
+  onError = "onError",
 }
 
 /**
@@ -21,31 +20,18 @@ interface PublisherEvents {
  * Stream real-time trades for US stocks, forex and crypto.
  * @see https://finnhub.io/docs/api#websocket-price
  */
-export class FinnhubWS {
-  private static _instance: FinnhubWS;
-
+export class FinnhubWS extends EventEmitter {
   private socket: WebSocket = null as any;
 
   private symbols: string[] = [];
 
-  public static get Instance() {
-    return this._instance || (this._instance = new this());
-  }
+  token: string = FINNHUB_KEY;
 
-  events: PublisherEvents = {} as any;
-
-  /**
-   * @template
-   * when/on
-   */
-  public when(
-    event: keyof PublisherEvents,
-    func: (data?: any) => Promise<any>,
-  ): void {
-    this.events[event] = func;
-  }
-
-  private constructor() {
+  constructor(token?: string) {
+    super();
+    if (token) {
+      this.token = token;
+    }
     this.config();
   }
 
@@ -59,10 +45,10 @@ export class FinnhubWS {
       if (checkIfMarketIsOpen()) {
         return self.init();
       } else {
-        console.log("Market is closed cannot subscribe to market data");
+        log("Market is closed cannot subscribe to market data");
         // infinity loop
         return setTimeout(() => {
-          console.log("FinnhubIO.config heartbeat");
+          log("FinnhubIO.config heartbeat");
           self.config();
         }, 5000);
       }
@@ -77,83 +63,63 @@ export class FinnhubWS {
   private init() {
     const self = this;
 
+    const token = self.token;
+
     // Emulate for test
-    // if (process.env.NODE_ENV === 'test') {
-    //     setTimeout(async () => {
+    if (process.env.NODE_ENV === "test") {
+      setTimeout(async () => {
+        self.emit(FinnhubWSEvents.onReady, true);
+      }, 3000);
+      return;
+    }
 
-    //         // Emit ready
-    //         const onReady = self.events["onReady"];
+    log("FinnhubIO.init startup", (token || "").slice(0, 5));
 
-    //         if (onReady) {
-    //             await onReady();
-    //         }
+    this.socket = new WebSocket(`wss://ws.finnhub.io?token=${token}`);
 
-    //     }, 3000);
-
-    //     return;
-    // }
-
-    console.log("FinnhubIO.init startup", (FINNHUB_KEY || "").slice(0, 5));
-
-    this.socket = new WebSocket(`wss://ws.finnhub.io?token=${FINNHUB_KEY}`);
-
-    this.socket.on("open", async function open(this: any) {
-      const onReady = self.events.onReady;
-      if (onReady) {
-        return onReady();
-      }
+    this.socket.on("open", () => {
+      self.emit(FinnhubWSEvents.onReady, true);
     });
 
-    this.socket.on("error", async (error: any) => {
-      console.log("on error connecting socket", error);
+    this.socket.on("error", (error: Error) => {
+      log("on error connecting socket", error);
 
-      const onError = self.events.error;
-      if (onError) {
-        return onError(error);
-      }
-
-      setTimeout(() => self.config(), 2000);
+      self.emit(FinnhubWSEvents.onError, error);
+      // setTimeout(() => self.config(), 2000);
       return;
     });
 
     interface OnSocketData {
-      data: [{ s: string; p: number; t: number; v: string }];
+      data: { s: string; p: number; t: number; v: string }[];
       type: string;
     }
 
-    this.socket.on(
-      "message",
-      async (data: OnSocketData): Promise<void> => {
-        // @ts-ignore
-        const parsedData: OnSocketData = JSONDATA(data);
+    this.socket.on("message", (data: OnSocketData): void => {
+      // @ts-ignore
+      const parsedData: OnSocketData = JSONDATA(data);
 
-        if (!parsedData) {
-          return;
-        }
+      if (!parsedData) {
+        return;
+      }
 
-        if (parsedData && parsedData.data && parsedData.data[0]) {
-          const priceItem = parsedData.data[0];
-          const symbol = priceItem.s;
+      if (parsedData && parsedData.data && parsedData.data[0]) {
+        const priceItem = parsedData.data[0];
+        const symbol = priceItem.s;
 
-          const dataToSend: TickData = {
-            price: priceItem.p,
-            date: new Date(priceItem.t),
-            symbol: priceItem.s,
-            volume: +priceItem.v,
-          };
+        const dataToSend: TickData = {
+          price: priceItem.p,
+          date: new Date(priceItem.t),
+          symbol: priceItem.s,
+          volume: +priceItem.v,
+        };
 
-          const topicSymbol = symbol;
+        const topicSymbol = symbol;
 
-          log(topicSymbol, dataToSend.price);
+        log(topicSymbol, dataToSend.price);
 
-          // If we have a publisher then send to it
-          const onData = self.events.onData;
-          if (onData) {
-            return onData(dataToSend);
-          }
-        }
-      },
-    );
+        self.emit(FinnhubWSEvents.onData, dataToSend);
+      }
+    });
   }
 
   /**
@@ -162,22 +128,21 @@ export class FinnhubWS {
    * @returns boolean
    */
   public addSymbol(symbol: string): boolean {
-    console.log("addSymbol", symbol);
+    log("addSymbol", symbol);
 
-    const onData = this.events.onData;
+    const self = this;
 
     // If test
     if (symbol === "TEST") {
-      setTimeout(async () => {
-        if (onData) {
-          const dataToSend: TickData = {
-            price: 1000,
-            date: new Date(),
-            symbol: "STQ",
-            volume: 0,
-          };
-          return onData(dataToSend);
-        }
+      setTimeout(() => {
+        const dataToSend: TickData = {
+          price: 1000,
+          date: new Date(),
+          symbol: "STQ",
+          volume: 0,
+        };
+
+        self.emit(FinnhubWSEvents.onData, dataToSend);
       }, 2000);
       return true;
     }
@@ -197,10 +162,10 @@ export class FinnhubWS {
       // Request market data for this symbol
       this.symbols.push(symbol);
       this.socket.send(JSON.stringify({ type: "subscribe", symbol }));
-      console.log("Added symbol", symbol);
+      log("Added symbol", symbol);
       return true;
     } catch (error) {
-      console.log("AddSybols ", error);
+      log("AddSybols ", error);
       return false;
     }
   }
@@ -221,7 +186,7 @@ export class FinnhubWS {
       }
       return false;
     } catch (error) {
-      console.log("Error removing symbol", error);
+      log("Error removing symbol", error);
       return false;
     }
   }
